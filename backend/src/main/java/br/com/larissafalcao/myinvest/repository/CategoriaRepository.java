9d1bd9b69e70ab0b7522b2ebd032bdb8a9a27eb1
Mathprestes <matheus.prestes_@outlook.com>
2021-05-25 18:39:11 -0300

    Primeiro commit backend

---
+++ backend/src/main/java/br/com/larissafalcao/myinvest/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package br.com.larissafalcao.myinvest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.larissafalcao.myinvest.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
---
+++ backend/src/main/java/br/com/larissafalcao/myinvest/resource/InvestimentoResource.java
@@ -0,0 +1,48 @@
+package br.com.larissafalcao.myinvest.resource;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.larissafalcao.myinvest.domain.Investimento;
+import br.com.larissafalcao.myinvest.repository.InvestimentoRepository;
+
+@RestController
+@RequestMapping ("/investimentos")
+public class InvestimentoResource {
+	
+	@Autowired
+	private InvestimentoRepository investimentoRepository;
+	
+	///////////////////////////////////////////////////
+	
+	//metodo listar
+	@GetMapping
+	public List<Investimento> listarTodos() {
+		return investimentoRepository.findAll();
+	}
+	
+	//metodo de busca
+	@GetMapping ("/{codigo}")   
+	public Investimento buscarPeloCodigo(@PathVariable Long codigo) {
+		return investimentoRepository.findById(codigo).orElse(null);
+	}
+	
+	//metodo de deletar
+	@DeleteMapping
+	public void remover(@PathVariable Long codigo) {
+		investimentoRepository.deleteById(codigo);
+	}
+	
+	//metodo de salvar
+	@PostMapping
+	public Investimento cadastrar(@RequestBody Investimento investimento) {
+		return investimentoRepository.save(investimento);
+		
+	}
+}
